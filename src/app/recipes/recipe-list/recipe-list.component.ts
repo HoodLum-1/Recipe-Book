1b0ffa03b95f591db3a8697123bbef220bf5aa41
Malesela Sithole <malesela.ms@gmail.com>
2019-12-07 14:23:56 +0200

    cmp-binding final commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A test Reciepe', 'This is simple a test','https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg'),
-    new Recipe('A test Reciepe', 'This is simple a test','https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg')
+    new Recipe('Another test Reciepe', 'This is simple a test','https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
