68ac2b815e5604df14d3a3e0f786a19f5f0e632e
Malesela Sithole <malesela.ms@gmail.com>
2019-12-11 23:46:54 +0200

    TD Approach (S-List)

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Oranges', 10)
@@ -11,6 +12,10 @@ export class ShoppingListService {
 
       getIngredients() {
           return this.ingredients.slice();
+      }
+
+      getIngredient(index: number) {
+        return this.ingredients[index];
       }
 
       addIngredient(ingredient: Ingredient) {
@@ -23,6 +28,16 @@ export class ShoppingListService {
         //     this.addIngredient(ingredient);
         // }
         this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      updateIngredient(index, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+      }
+
+      deleteIngedient(index: number) {
+        this.ingredients.splice(index, 1);
         this.ingredientsChanged.next(this.ingredients.slice());
       }
 }
