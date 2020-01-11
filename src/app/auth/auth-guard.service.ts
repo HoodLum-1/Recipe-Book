85ae2675322b5f6da2a9beec06f7741086c0d027
Malesela Sithole <malesela.ms@gmail.com>
2020-01-11 21:16:26 +0200

    basic-auth-setup

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router/src/utils/preactivation';
-import { AuthService } from './auth.service';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './store/auth.reducers';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService) {}
+    constructor(private store: Store<fromApp.AppState>) {}
     
     path: ActivatedRouteSnapshot[];
     route: ActivatedRouteSnapshot;
 
     canActivate(path: ActivatedRouteSnapshot, route: RouterStateSnapshot) {
-            return this.authService.isAuthenticated();
+            return this.store.select('auth').pipe(
+                map(
+                    (authState: fromAuth.State) => {
+                        return authState.authenticated;
+                    }
+                )
+            );
         }
-}
+}   
---
+++ src/app/auth/store/auth.actions.ts
@@ -0,0 +1,26 @@
+import { Action } from '@ngrx/store';
+
+export const SIGNUP = 'SINGUP';
+export const SIGNIN = 'SIGNIN';
+export const LOGOUT = 'LOGOUT';
+export const SET_TOKEN = 'SET_TOKEN';
+
+export class Signup implements Action {
+    readonly type = SIGNUP;
+}
+
+export class Singin implements Action {
+    readonly type = SIGNIN;
+}
+
+export class Logout implements Action {
+    readonly type = LOGOUT;
+}
+
+export class SetToken implements Action {
+    readonly type = SET_TOKEN;
+
+    constructor(public payload: string) {}
+}
+
+export type AuthActions = Signup | Singin | Logout | SetToken;
